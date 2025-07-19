ec3193f02b29e6f12bd45e2490dcb2e83d9f0c31
Fatima Ramoni <olabisifat@gmail.com>
2025-07-19 10:56:13 +0100

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,16 @@
-import Header from './components/Header';
+/*import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default App;*/
+
+/*import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
@@ -12,4 +24,22 @@ function App() {
   );
 }
 
+export default App;*/
+
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </>
+  );
+}
+
 export default App;
+
+
