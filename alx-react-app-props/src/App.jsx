259c64d8d475b2f849622dc26a8a65a0a345c75b
Fatima Ramoni <olabisifat@gmail.com>
2025-07-20 18:36:07 +0100

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,64 @@
+/*import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default App;*/
+
+/*import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;*/
+
+/*import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </>
+  );
+}
+
+export default App;*/
+
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+    
+
+  );
+}
+
+export default App;
+
+
+
