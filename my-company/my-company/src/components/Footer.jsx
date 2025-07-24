ce867768ada2c80d10f260c9c41720206e2fd39e
Fatima Ramoni <olabisifat@gmail.com>
2025-07-24 17:58:03 +0100

    Build a Simple Company Website with React

---
+++ my-company/my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#eee', padding: '10px', textAlign: 'center' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
