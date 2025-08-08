4df2ba6f7ff9054d310b4fd21864c96c18b4d8e4
Fatima Ramoni <olabisifat@gmail.com>
2025-08-08 17:56:28 +0100

    Add App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,30 @@
-
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import SearchBar from './components/SearchBar';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1 style={{ textAlign: 'center' }}>Recipe Sharing App</h1>
+        <SearchBar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
